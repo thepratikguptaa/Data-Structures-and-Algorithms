9749d0c3851121e464dc26dbcd743da7283de18d
Pratik Gupta <pratikguptamain@gmail.com>
2026-01-26 21:51:09 +0530

    Add solution for the Product of Array Except Self problem

---
+++ leetcodeProblems/prob-238.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 1);
+        
+        // First pass: Calculate left products
+        // result[i] will contain product of all elements to the left of i
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        
+        // Second pass: Calculate right products and multiply with left products
+        // rightProduct tracks the product of all elements to the right of current index
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+        
+        return result;
+    }
+};
